084c9488d79b1d036ecca8585224c0c1659f0e82
NietThijmen <53520119+NietThijmen@users.noreply.github.com>
2024-03-19 20:19:20 +0100

    feat! updated database.sql

--- database.sql
+++ database.sql
@@ -1,4 +1,12 @@
-CREATE TABLE IF NOT EXISTS`tavern_tokens`(
+CREATE TABLE `tavern_images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `vault_id` int(11) DEFAULT NULL,
+  `path` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT curtime(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=266 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `tavern_tokens` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `vault_id` int(11) NOT NULL,
   `token` varchar(255) DEFAULT NULL,
