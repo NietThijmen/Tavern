1d02c29d8355c1a0c21c6c92570509b0a095761b
NietThijmen <53520119+NietThijmen@users.noreply.github.com>
2024-03-19 09:40:31 +0100

    feat! change build to also build 32 bit

--- build.sh
+++ build.sh
@@ -1,11 +1,17 @@
 # bin/bash
 echo "Building compiled versions..."
 
-echo "Building for Windows"
-env GOOS=windows GOARCH=amd64 go build -o bin/windows.exe .
+echo "Building for Windows (64-bit)"
+env GOOS=windows GOARCH=amd64 go build -o bin/windows-64.exe .
 
-echo "Building for Linux"
+echo "Building for Windows (32-bit)"
+env GOOS=windows GOARCH=386 go build -o bin/windows-32
+
+echo "Building for Linux (64-bit)"
 env GOOS=linux GOARCH=amd64 go build -o bin/linux .
+
+echo "Building for Linux (32-bit)"
+env GOOS=linux GOARCH=386 go build -o bin/linux-32 .
 
 echo "Building for Mac (Intel)"
 env GOOS=darwin GOARCH=amd64 go build -o bin/mac_intel .
