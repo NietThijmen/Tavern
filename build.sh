9fd39b43bb4febff838b800a4792cecf17e5dba8
NietThijmen <53520119+NietThijmen@users.noreply.github.com>
2024-03-19 09:37:05 +0100

    feat! change build names

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 # bin/bash
-env GOOS=windows GOARCH=amd64 go build -o bin/build.exe .
-env GOOS=linux GOARCH=amd64 go build -o bin/build .
-env GOOS=darwin GOARCH=amd64 go build -o bin/build_mac .
+echo "Building compiled versions..."
 
-env GOOS=darwin GOARCH=arm64 go build -o bin/build_mac_arm .
+echo "Building for Windows"
+env GOOS=windows GOARCH=amd64 go build -o bin/windows.exe .
+
+echo "Building for Linux"
+env GOOS=linux GOARCH=amd64 go build -o bin/linux .
+
+echo "Building for Mac (Intel)"
+env GOOS=darwin GOARCH=amd64 go build -o bin/mac_intel .
+
+echo "Building for Mac (Apple Silicon)"
+env GOOS=darwin GOARCH=arm64 go build -o bin/mac_arm .
