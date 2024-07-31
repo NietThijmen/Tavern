90a6150c5e799e8b7c338b6a1242219845fb632c
NietThijmen <53520119+NietThijmen@users.noreply.github.com>
2024-07-31 20:24:52 +0200

    chore! bye bye codebase

--- go.mod
+++ go.mod
@@ -1,22 +1,3 @@
 module github.com/nietthijmen/tavern
 
 go 1.21
-
-require (
-	github.com/go-sql-driver/mysql v1.8.0
-	github.com/google/uuid v1.6.0
-	github.com/nurlantulemisov/imagecompression v0.0.0-20211028165702-e399758d3838
-	github.com/prometheus/client_golang v1.19.0
-)
-
-require (
-	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
-	github.com/prometheus/client_model v0.5.0 // indirect
-	github.com/prometheus/common v0.48.0 // indirect
-	github.com/prometheus/procfs v0.12.0 // indirect
-	golang.org/x/sys v0.16.0 // indirect
-	gonum.org/v1/gonum v0.15.0 // indirect
-	google.golang.org/protobuf v1.32.0 // indirect
-)
--- main.go
+++ main.go
@@ -1,46 +1,5 @@
 package main
 
-import (
-	"github.com/nietthijmen/tavern/src/config"
-	"github.com/nietthijmen/tavern/src/optimisation"
-	"github.com/nietthijmen/tavern/src/prometheus"
-	"github.com/nietthijmen/tavern/src/routes"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"log"
-	"net/http"
-	"runtime"
-	"strings"
-)
-
 func main() {
-	domain := config.ReadEnv("DOMAIN", "localhost")
-	port := config.ReadEnv("PORT", "8080")
-	enablePrometheus := config.ReadEnv("ENABLE_PROMETHEUS", "false") == "true"
-
-	log.Println("Starting queue server for optimisation")
-	optimisation.StartQueueThread()
-
-	// start a http server
-	http.Handle("/storage/", http.StripPrefix("/storage/", http.FileServer(http.Dir("storage"))))
-	http.HandleFunc("/", routes.Upload)
-
-	if enablePrometheus {
-		prometheus.RecordMetrics()
-		http.Handle("/metrics", promhttp.Handler())
-	}
-
-	var listen string
-	if strings.Contains(runtime.GOOS, "windows") {
-		listen = "127.0.0.1:" + port
-	} else {
-		listen = ":" + port
-	}
-
-	log.Printf("Starting server on host: %s & port %s.", domain, port)
-	err := http.ListenAndServe(listen, nil)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
 
 }
